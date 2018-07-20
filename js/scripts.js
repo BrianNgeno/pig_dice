434b6c0fc334e907cb27c5277562440541ad583a
brianngeno <bkn.ngeno@gmail.com>
2018-07-20 15:05:57 +0300

    added prototype reset and prototype switch

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,19 @@ function Player(playername, turn) {
   this.score = 0
   this.totalroll = 0
   this.turn = turn
-}
+};
 Player.prototype.roll = function() {
   document.getElementById("demo").innerHTML =
     Math.floor(Math.random() * 6);
-}
+};
+Player.prototype.swith = function() {
+    if (turn === true) {
+      this.turn = false
+    } else {
+      this.turn = true
+    }
+  };
+    Player.prototype.reset = function() {
+      this.score = 0
+      this.totalroll = 0
+    }
