27c3867c0a82993b5bda7c7ba049b1bb1c144826
brianngeno <bkn.ngeno@gmail.com>
2018-07-20 16:14:24 +0300

    hide the name input form

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,30 @@
-var player1;
-var player2;
-
-function Player(playername, turn) {
-  this.playername = playername
-  this.score = 0
-  this.totalroll = 0
-  this.turn = turn
-};
-Player.prototype.roll = function() {
-  document.getElementById("demo").innerHTML =
-    Math.floor(Math.random() * 6);
-};
-Player.prototype.swith = function() {
-    if (turn === true) {
-      this.turn = false
-    } else {
-      this.turn = true
-    }
-  };
-    Player.prototype.reset = function() {
-      this.score = 0
-      this.totalroll = 0
-    }
+// var player1;
+// var player2;
+//
+// function Player(playername, turn) {
+//   this.playername = playername
+//   this.score = 0
+//   this.totalroll = 0
+//   this.turn = turn
+// };
+// Player.prototype.roll = function() {
+//   document.getElementById("demo").innerHTML =
+//     Math.floor(Math.random() * 6);
+// };
+// Player.prototype.swith = function() {
+//   if (turn === true) {
+//     this.turn = false
+//   } else {
+//     this.turn = true
+//   }
+// };
+// Player.prototype.reset = function() {
+//   this.score = 0
+//   this.totalroll = 0
+// };
+$(document).ready(function(){
+  $("#lets-play").click(function(event){
+    $("#input-one").hide();
+    event.preventDefault();
+  });
+});
