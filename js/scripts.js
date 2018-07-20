e9795fbf45af7d744b76021e59f47f955b694d2b
brianngeno <bkn.ngeno@gmail.com>
2018-07-20 17:45:52 +0300

    displayed player 2 name

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,34 @@
-// var player1;
-// var player2;
-//
-// function Player(playername, turn) {
-//   this.playername = playername
-//   this.score = 0
-//   this.totalroll = 0
-//   this.turn = turn
-// };
-// Player.prototype.roll = function() {
-//   document.getElementById("demo").innerHTML =
-//     Math.floor(Math.random() * 6);
-// };
-// Player.prototype.swith = function() {
-//   if (turn === true) {
-//     this.turn = false
-//   } else {
-//     this.turn = true
-//   }
-// };
-// Player.prototype.reset = function() {
-//   this.score = 0
-//   this.totalroll = 0
-// };
+var player1;
+var player2;
+
+function Player(playername, turn) {
+  this.playername = playername
+  this.score = 0
+  this.totalroll = 0
+  this.turn = turn
+};
+Player.prototype.roll = function() {
+  document.getElementById("demo").innerHTML =
+    Math.floor(Math.random() * 6);
+};
+Player.prototype.swith = function() {
+  if (turn === true) {
+    this.turn = false
+  } else {
+    this.turn = true
+  }
+};
+Player.prototype.reset = function() {
+  this.score = 0
+  this.totalroll = 0
+};
 $(document).ready(function(){
   $("#lets-play").click(function(event){
     $("#input-one").hide();
     event.preventDefault();
   var player1name=  $("input#inputed-line1").val();
     $("#player1name").text(player1name);
+    var player2name=  $("input#inputed-line2").val();
+      $("#player2name").text(player2name)
   });
 });
